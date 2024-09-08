99993c8638557fb74f3daff62ca640005baebb3f
yoruck2 <yoruck2@gmail.com>
2024-09-09 00:48:15 +0900

    🐛 더미데이터 오타 수정

--- CoInfo/Search/SearchView.swift
+++ CoInfo/Search/SearchView.swift
@@ -7,25 +7,22 @@
 
 import SwiftUI
 
-
-
-struct ContentView: View {
+struct SearchView: View {
     
     @State var text = ""
     @State var coinList: [Search.Coin] = [
-        Search.Coin(id: "BitCoin",
+        Search.Coin(id: "bitcoin",
                     name: "BitCoin",
                     symbol: "BTC",
-                    thumb: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png"),
+                    thumb: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png", large: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png"),
         Search.Coin(id: "ethereum",
                     name: "Ethereum",
                     symbol: "ETH",
-                    thumb: "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png"),
+                    thumb: "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png", large: "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png"),
         Search.Coin(id: "ginnan-the-cat",
                     name: "Ginnan The Cat",
                     symbol: "GINNAN",
-                    thumb: "https://coin-images.coingecko.com/coins/images/39466/standard/ginnan_logo_final.png?1724116323"),
-        
+                    thumb: "https://coin-images.coingecko.com/coins/images/39466/standard/ginnan_logo_final.png?1724116323", large: "https://coin-images.coingecko.com/coins/images/39466/standard/ginnan_logo_final.png?1724116323")
     ]
     
     var searchedCoin: [Search.Coin] {
@@ -41,11 +38,12 @@ struct ContentView: View {
             .searchable(text: $text)
         }
     }
+    
     func listView() -> some View {
         LazyVStack {
             ForEach(searchedCoin, id: \.id) { item in
                 NavigationLink {
-                    CoinDetailView()
+                    CoinDetailView(coinData: item)
                 } label: {
                     CoinRowView(coinData: item)
                 }
@@ -57,45 +55,6 @@ struct ContentView: View {
     }
 }
 
-struct CoinRowView: View {
-    
-    let coinData: Search.Coin
-    
-    var body: some View {
-        HStack {
-            AsyncImage(url: URL(string: coinData.thumb)) { phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                case .success(let image):
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40, height: 40)
-                case .failure:
-                    Image(systemName: "photo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                @unknown default:
-                    EmptyView()
-                }
-            }
-            VStack(alignment: .leading) {
-                Text(coinData.name)
-                    .fontWeight(.bold)
-                Text(coinData.symbol)
-                    .font(.system(size: 15))
-            }
-            Spacer()
-            Button {
-                print("tap")
-            } label: {
-                Image(systemName: "star")
-            }
-        }.padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 10))
-    }
-}
 #Preview {
-    ContentView()
+    CoInfoTabView()
 }
