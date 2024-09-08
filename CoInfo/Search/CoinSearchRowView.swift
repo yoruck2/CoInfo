29496f49142ddec68a58ac0c948d414f0b0e5aa8
yoruck2 <yoruck2@gmail.com>
2024-09-09 00:46:39 +0900

    ✨ CoinRowView 구현

--- CoInfo/Search/CoinSearchRowView.swift
+++ CoInfo/Search/CoinSearchRowView.swift
@@ -7,12 +7,47 @@
 
 import SwiftUI
 
-struct CoinSearchRowView: View {
+struct CoinRowView: View {
+    
+    let coinData: Search.Coin
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string: coinData.thumb)) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                case .failure:
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            .frame(width: 40, height: 40)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 5))
+            
+            VStack(alignment: .leading) {
+                Text(coinData.name)
+                    .fontWeight(.bold)
+                Text(coinData.symbol)
+                    .font(.system(size: 15))
+            }
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 10))
+        .contentShape(Rectangle())
+        Button {
+            print("tap")
+        } label: {
+            Image(systemName: "star")
+        }
     }
-}
-
-#Preview {
-    CoinSearchRowView()
 }
